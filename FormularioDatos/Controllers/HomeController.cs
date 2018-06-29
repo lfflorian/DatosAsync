957d1b0ee901dc3ae81fd422a71bc82d90983bf8
Luis Fernando Florian <lflorian@mega.corp>
2018-06-29 16:17:40 -0600

    Proyecto

---
+++ FormularioDatos/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using FormularioDatos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormularioDatos.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Formulario()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Formulario(Datos datos)
+        {
+
+            ViewBag.Message = "Your contact page.";
+
+            return View("datos de resultado");
+        }
+        
+    }
+}
---
+++ FormularioDatos/Models/Datos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FormularioDatos.Models
+{
+    public class Datos
+    {
+        public string PrimerDato { get; set; }
+        public string SegundoDato { get; set; }
+        public string TercerDato { get; set; }
+    }
+}
